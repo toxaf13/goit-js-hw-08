7f406151aca173ea0e78e57277be6e7b65824ed4
wombatstar <toxaf13@gmail.com>
2022-07-31 18:30:06 +0200

    2,1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,32 +1,32 @@
+// Add imports above this line
+import { galleryItems } from './gallery-items';
+// Change code below this line
+
+console.log(galleryItems);
+
 // Opisany w dokumentacji
 import SimpleLightbox from 'simplelightbox';
 // Dodatkowy import stylów
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
 import { galleryItems } from './gallery-items.js';
+// Change code below this line
 
-const gallery = document.querySelector(".gallery");
+const gallery = document.querySelector('.gallery');
 
-const galleryItem = galleryItems.map((galleryItem) => 
-`<div class="gallery__item">
-   <a class="gallery__link" href="${galleryItem.original}">
-      <img class="gallery__image"
-         src="${galleryItem.preview}"
-         data-source= "${galleryItem.original}" 
-         alt="${galleryItem.description}"
-      />
-   </a>
-</div>`).join("");
+const galleryItem = galleryItems
+  .map(
+    galleryItem => `
+<a class="gallery__item" href="${galleryItem.original}">
+  <img
+  class="gallery__image"
+  src="${galleryItem.preview}"
+  alt="${galleryItem.description}" 
+  title = "${galleryItem.description}"/>
+</a>`
+  )
+  .join('');
 
 gallery.innerHTML = galleryItem;
 
-gallery.addEventListener("click", (event) => {
-   event.preventDefault();
-
-   const dataImg = event.target.getAttribute("data-source");
-   const instance = basicLightbox.create(`
-   <img src="${dataImg}" width="800" height="600">
-`);
-
-instance.show()
-});
+new SimpleLightbox('.gallery a', { captionDelay: 250 });
